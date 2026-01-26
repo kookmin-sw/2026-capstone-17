ba662c9f3983308dd670a2260ee9c3c9dcbf1a14
Seungho-Min <84025029+Seungho-Min@users.noreply.github.com>
2026-01-26 22:19:25 +0900

    initial commit

---
+++ src/main/kotlin/com/capstone/focus/FocusApplication.kt
@@ -0,0 +1,11 @@
+package com.capstone.focus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FocusApplication
+
+fun main(args: Array<String>) {
+	runApplication<FocusApplication>(*args)
+}
