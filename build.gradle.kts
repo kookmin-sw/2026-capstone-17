0c59e5ff7c105a3be63eea30b7f3188faa660882
liojj-tech <lio.jj@kakaomobility.com>
2026-01-27 16:20:10 +0900

    chore: add grpc (protobuf)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,19 @@
+import com.google.protobuf.gradle.id
+
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("jvm") version "2.3.0"
+	kotlin("plugin.spring") version "2.3.0"
 	id("org.springframework.boot") version "4.0.2"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "2.2.21"
+	id("com.google.protobuf") version "0.9.5"
+	kotlin("plugin.jpa") version "2.3.0"
 }
 
 group = "com.capstone"
 version = "0.0.1-SNAPSHOT"
 description = "Capstone project focus"
+
+extra["springGrpcVersion"] = "1.0.1"
 
 java {
 	toolchain {
@@ -28,7 +33,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.grpc:spring-grpc-server-web-spring-boot-starter")
+	implementation("io.grpc:grpc-services")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	compileOnly("org.projectlombok:lombok")
@@ -40,9 +48,35 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.grpc:spring-grpc-dependencies:${property("springGrpcVersion")}")
+	}
+}
+
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc") {
+					option("@generated=omit")
+				}
+			}
+		}
 	}
 }
 
